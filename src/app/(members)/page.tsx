3fbb1e2e756792826198db5f919b13c208c79620
박형석 <baghyeongseog@baghyeongseog-ui-MacBookAir.local>
2025-06-01 23:49:26 +0900

    client member page

--- src/app/(members)/page.tsx
+++ src/app/(members)/page.tsx
@@ -1,34 +1,5 @@
-"use client";
-
-import { useSelectedMemberStore } from "@/stores/selectedMemberStore";
-import MemberList from "./_components/MemberList";
-import MemberDetail from "./_components/MemberDatail";
-import ProtectedPage from "@/components/ProtectedPage";
+import ClientMemberPage from "@/components/ClientMemberPage";
 
 export default function MemberPage() {
-  const { selectedMember } = useSelectedMemberStore();
-
-  return (
-    <ProtectedPage>
-      <div className="relative h-screen flex md:flex-row">
-        {/* 리스트 영역 */}
-        <div className="w-full md:w-1/5 overflow-y-auto scrollbar-hide">
-          <MemberList />
-        </div>
-
-        {/* 데스크탑 전용 상세 영역 */}
-        {selectedMember && (
-          <div className="w-full md:w-4/5 hidden md:block overflow-y-auto">
-            <MemberDetail />
-          </div>
-        )}
-
-        {selectedMember && (
-          <div className="absolute inset-0 z-50 md:hidden">
-            <MemberDetail />
-          </div>
-        )}
-      </div>
-    </ProtectedPage>
-  );
+  return <ClientMemberPage />;
 }
